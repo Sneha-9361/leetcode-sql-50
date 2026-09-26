9da673fd8c15bf657e685aae42537703bcb65c0d
Sneha K <snehakumaravel6@gmail.com>
2026-09-26 17:34:25 +0530

    update: save all modified files

--- 01_select/1757_recyclable_products.sql
+++ 01_select/1757_recyclable_products.sql
@@ -186,3 +186,18 @@ u.user_id=r.user_id
 
 group by r.contest_id
 order by percentage desc,r.contest_id ;
+
+
+
+
+problem:19
+problem:queries quality
+link:https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
